764aff86627dc1c11e96b9984ab781529edca8b2
SunethChinthaka <sunethedu@gmail.com>
2022-09-06 11:48:28 +0530

    SOAP Example

---
+++ SOAP-WS-Example/src/main/java/SimpleCal.java
@@ -0,0 +1,19 @@
+
+public class SimpleCal {
+	public float addNum(float a, float b) {
+		return a + b;
+	}
+
+	public float subNum(float a, float b) {
+		return a - b;
+	}
+
+	public float mulNum(float a, float b) {
+		return a * b;
+	}
+
+	public float divNum(float a, float b) {
+		return a / b;
+	}
+
+}
