92bdce61bf7262399013de60f1bec98d1fd8defd
SunethChinthaka <sunethedu@gmail.com>
2022-10-28 17:20:13 +0530

    Angular + SSO App

---
+++ sso-angular/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authConfig } from './sso.config';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+  title = 'sso-angular';
+
+  login(){
+   this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
+}
